82f58cdcd69f74f532d27275b1991c6de4eee237
Saswat <87571040+SaswatJK@users.noreply.github.com>
2025-09-06 15:34:54 +0545

    worked on the architecture of the image where the scene is saved

--- source/main.go
+++ source/main.go
@@ -1,11 +1,18 @@
 package main
 
 import "fmt"
+import "image"
+import "image/color"
+import "image/jpeg"
+import "os"
+
+const IMG_WIDTH = 500
+const IMG_HEIGHT = 500
 
 type vec3 struct {
-	a float32
+	r float32
+	g float32
 	b float32
-	c float32
 }
 
 type Camera struct {
@@ -13,6 +20,40 @@ type Camera struct {
 	direction vec3
 }
 
+type Scene struct {
+	sceneImage *image.RGBA
+	camera     *Camera
+}
+
+func initializeScene() *Scene {
+	return &Scene{
+		sceneImage: image.NewRGBA(image.Rect(0, 0, IMG_WIDTH, IMG_HEIGHT)),
+		camera: &Camera{
+			position:  vec3{0.0, 0.0, 0.0},
+			direction: vec3{0.0, 0.0, -1.0},
+		},
+	}
+}
+
+func colorScene(currScene *Scene) { //Color it white for now.
+	var pixelColor color.Color
+	pixelColor = color.RGBA{125, 100, 230, 255}
+	for i := 0; i < 25; i++ {
+		currScene.sceneImage.Set(i, i, pixelColor)
+	}
+}
+
 func main() {
+
+	currentScene := initializeScene()
+	colorScene(currentScene)
+	file, err := os.Create("output.jpg") //Since this will already be in the build directory, don't need to do relative path.
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	err = jpeg.Encode(file, currentScene.sceneImage, &jpeg.Options{Quality: 100})
 	fmt.Println("Hello world")
 }
