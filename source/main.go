d52056fa56c575eceb3a8d0b8ff21b5fcb1960e1
Saswat <87571040+SaswatJK@users.noreply.github.com>
2025-09-08 11:08:45 +0545

    direction ray seems to work

--- source/main.go
+++ source/main.go
@@ -17,10 +17,54 @@ type vec3 struct {
 	b float32
 }
 
+func add(v1, v2 vec3) vec3 {
+	var temp vec3
+	temp.r = v1.r + v2.r
+	temp.g = v1.g + v2.g
+	temp.b = v1.b + v2.b
+	return temp
+}
+
+func negate(v vec3) vec3 {
+	var temp vec3
+	temp.r = -v.r
+	temp.g = -v.g
+	temp.b = -v.b
+	return temp
+}
+
+func sub(v1, v2 vec3) vec3 {
+	var nv2 vec3
+	nv2 = v2
+	nv2 = negate(nv2)
+	temp := add(v1, nv2)
+	return temp
+}
+
+func mulWithScalar(v vec3, s float32) vec3 {
+	var temp vec3
+	temp.r = v.r * s
+	temp.g = v.g * s
+	temp.b = v.b * s
+	return temp
+}
+
+func dot(v1, v2 vec3) float32 {
+	return (v1.r*v2.r + v1.b*v2.b + v1.g*v2.g)
+}
+
+func clampMax(value float32, clampValue float32) float32 {
+	if value < clampValue {
+		return clampValue
+	}
+	return value
+}
+
 type Camera struct {
-	position  vec3
-	direction vec3
-	right     vec3
+	position vec3
+	view     vec3
+	right    vec3
+	up       vec3
 }
 
 type Scene struct {
@@ -38,9 +82,10 @@ func initializeScene() *Scene {
 	return &Scene{
 		sceneImage: image.NewRGBA(image.Rect(0, 0, VIEWPORT_WIDTH, VIEWPORT_HEIGHT)),
 		camera: &Camera{
-			position:  vec3{0.0, 0.0, 0.0},
-			direction: vec3{0.0, 0.0, -1.0},
-			right:     vec3{1.0, 0.0, 0.0},
+			position: vec3{0.0, 0.0, 0.0},
+			view:     vec3{0.0, 0.0, -1.0},
+			right:    vec3{1.0, 0.0, 0.0},
+			up:       vec3{0.0, 1.0, 0.0},
 		},
 	}
 }
@@ -51,17 +96,20 @@ func colorScene(currScene *Scene) { //Color it white for now.
 	for i := 0; i < VIEWPORT_HEIGHT; i++ {
 		for j := 0; j < VIEWPORT_WIDTH; j++ {
 			var x, y float32 //Basically they are the normalized coordinates??
-			//Firstly we convert these things to 0-1 normalization. 0.5 is for pointing to the fake 'center' of the pixel.
-			x = (float32(j) + 0.5) / float32(VIEWPORT_WIDTH)
-			y = (float32(i) + 0.5) / float32(VIEWPORT_HEIGHT)
-			//Convert the 0-1 to NDC (-1 to +1).
-			x = (2.0*x - 1.0)
-			y = (2.0*y - 1.0)
-			//Converting this to the real world coordinates now.
-			x = (float32(IMAGE_WIDTH) * 0.5)
-			y = (float32(IMAGE_HEIGHT) * 0.5)
-			//Now we need to convert this to a ray. We will need an 'FOV', the camera direction, which we already have, and the camera position.
-			currScene.sceneImage.Set(i, i, pixelColor)
+			//q(t) = o + td
+			x = (2.0 * (float32(j) + 0.5) / float32(VIEWPORT_WIDTH)) - 1.0
+			y = (2.0 * (float32(i) + 0.5) / float32(VIEWPORT_HEIGHT)) - 1.0
+			//s(x,y) = a*f*x*r - f*y*u + v || u = up || v = view || a = aspect ratio = 1 || r = right || f = focal length = tan(phi/2) ||
+			//d = normalized (s)
+			var rayDir vec3
+			rayDir = sub(mulWithScalar(currScene.camera.right, x), mulWithScalar(currScene.camera.up, y))
+			rayDir = add(rayDir, currScene.camera.view)
+			var posRayDir vec3
+			posRayDir.r = clampMax(rayDir.r, 0.0)
+			posRayDir.g = clampMax(rayDir.g, 0.0)
+			posRayDir.b = clampMax(rayDir.b, 0.0)
+			pixelColor = color.RGBA{uint8(posRayDir.r * 255), uint8(posRayDir.g * 255), uint8(posRayDir.b * 255), 255}
+			currScene.sceneImage.Set(j, i, pixelColor)
 		}
 	}
 }
